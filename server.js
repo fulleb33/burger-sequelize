46a05eb904f0941a873b174c9f5bad73fd77451b
b f <b f>
2017-08-12 20:58:08 -0500

    fixes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var db = require("./models");
 var mysql = require("mysql");
 
 var app = express();
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(process.cwd() + "/public"));
 
